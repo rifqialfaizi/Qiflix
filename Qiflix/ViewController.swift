7867b048b0fd6d7f29c5b66e2aab042e0a5cf1cf
rifqialfaizi <rifqialfaizii@gmail.com>
2020-06-11 07:51:48 +0700

    Initial Commit

---
+++ Qiflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qiflix
+//
+//  Created by Rifqi Alfaizi on 11/06/20.
+//  Copyright © 2020 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
