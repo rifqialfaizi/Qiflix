b3c3a92b74cf4734fd32a9ee073092db052c9933
rifqialfaizi <rifqialfaizii@gmail.com>
2020-06-11 08:26:47 +0700

    Add TableViewCell

---
+++ Qiflix/Cell/CategoryMovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryMovieCell.swift
+//  Qiflix
+//
+//  Created by Rifqi Alfaizi on 11/06/20.
+//  Copyright © 2020 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class CategoryMovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
